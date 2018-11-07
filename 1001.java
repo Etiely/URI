0acf618ad5d07ee16557f564db76702893034311
Etiely Martins <38673280+Etiely@users.noreply.github.com>
2018-11-07 06:32:57 -0200

    Add files via upload

---
+++ 1001.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[]args) {
+		
+		int A, B, X;
+		
+		Scanner t = new Scanner(System.in);
+		
+		A = t.nextInt();
+		
+		B = t.nextInt();
+		
+		X = A + B;
+		
+		System.out.println("X = " + X);
+	
+	}
+
+}
